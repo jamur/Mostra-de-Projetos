faff5dbaae72b38c58fab9a68ef09b580997647c
Rafael Jamur <rjamur@gmail.com>
2009-09-25 00:09:47 -0300

    protótipo pronto

--- cadproj/admin.py
+++ cadproj/admin.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from cadproj.models import Pessoa, Estudante, Orientador, Projeto, Curso, TipoDeProjeto, PalavraChave, ModoDeApresentacao, Local, Cidade
+from cadproj.models import Pessoa, Estudante, Orientador, Projeto, Curso, TipoDeProjeto, PalavraChave, ModoDeApresentacao, Local, Cidade, Recurso
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
@@ -20,24 +20,34 @@ class OrientadorOptions(admin.ModelAdmin):
 
 class ProjetoOptions(admin.ModelAdmin):
     #inlines = [Pendencia_Inline, Contatamento_Inline]
-    list_display = ('titulo', 'orientador')
+    list_display = ('titulo', 'estudante', 'orientador')
     fieldsets = (
-        (None, {'fields': (
-                'titulo','descricao','estudante','orientador','curso')}),
-        ('Projeto', ('fields': ('tipo_de_projeto','tipo_exotico','palavra_chave')}),
-        ('Abrangência', ('fiels': ('cidade_de_abrangencia','local_de_abrangencia')}),
-        ('Apresentação', ('fields': ('modo_de_apresentacao','materiais_para_a_apresentacao','outro_modo')}),
-        )
+        (None, {
+            'fields': ('titulo','descricao','estudante','orientador','curso')
+        }),
+        ('Projeto', {
+            'fields': ('tipo_de_projeto','tipo_exotico','palavra_chave')
+        }),
+        ('Abrangência', {'fields':('cidade_de_abrangencia','local_de_abrangencia')
+        }),
+        ('Apresentação', {'fields':('modo_de_apresentacao','materiais_para_a_apresentacao','recurso_para_a_apresentacao','outro_modo')
+        }),
+    )
     list_per_page = 25
     search_fields = ['titulo', 'descricao']
-    list_filter = ('orientador','estudante')
+    list_filter = ('orientador','curso','tipo_de_projeto','cidade_de_abrangencia','local_de_abrangencia')
 
-admin.site.register(Pessoa, PessoaOptions)
-admin.site.register(Estudante, EstudanteOptions)
-admin.site.register(Orientador, OrientadorOptions)
-admin.site.register(Projeto, ProjetoOptions)
 admin.site.register(Curso)
 admin.site.register(TipoDeProjeto)
 admin.site.register(PalavraChave)
 admin.site.register(ModoDeApresentacao)
+
+admin.site.register(Pessoa, PessoaOptions)
+admin.site.register(Estudante, EstudanteOptions)
+admin.site.register(Orientador, OrientadorOptions)
+
+admin.site.register(Projeto, ProjetoOptions)
+admin.site.register(Cidade)
+admin.site.register(Local)
+admin.site.register(Recurso)
 
--- cadproj/models.py
+++ cadproj/models.py
@@ -52,22 +52,28 @@ class Local(models.Model):
     def __unicode__(self):
         return self.local
 
+class Recurso(models.Model):
+    recurso = models.CharField(max_length = 50)
+
+    def __unicode__(self):
+        return self.recurso
+
 class Projeto(models.Model):
     titulo = models.CharField('Título do Projeto', max_length = 150) 
     descricao = models.TextField('Descrição do Projeto')
-    estudante = models.ManyToManyField(Estudante, related_name='projetos')
+    estudante = models.ForeignKey(Estudante, related_name='projetos')
     orientador = models.ForeignKey(Orientador)
-    #coparticipante = models.ManyToManyField(Estudante, related_name='projs')
-    curso = models.ForeignKey(Curso)
-    tipo_de_projeto = models.ForeignKey(TipoDeProjeto)
-    tipo_exotico = models.CharField(max_length = 50)
+    coparticipante = models.ManyToManyField(Estudante, null=True, blank=True) #, related_name='projs')
+    curso = models.ForeignKey(Curso, null=True, blank=True)
+    tipo_de_projeto = models.ForeignKey(TipoDeProjeto, null=True, blank=True)
+    tipo_exotico = models.CharField(max_length = 50, null=True, blank=True)
     palavra_chave = models.ManyToManyField(PalavraChave)
-    modo_de_apresentacao = models.ForeignKey(ModoDeApresentacao)
-    outro_modo = models.TextField()
-    cidade_de_abrangencia = models.ManyToManyField(Cidade)
-    local_de_abrangencia = models.ManyToManyField(Local)
-    materias_para_a_apresentacao = models.TextField()
-
+    modo_de_apresentacao = models.ForeignKey(ModoDeApresentacao, null=True, blank=True)
+    outro_modo = models.TextField(null=True, blank=True)
+    cidade_de_abrangencia = models.ManyToManyField(Cidade, null=True, blank=True)
+    local_de_abrangencia = models.ManyToManyField(Local, null=True, blank=True)
+    materiais_para_a_apresentacao = models.TextField(null=True, blank=True)
+    recurso_para_a_apresentacao = models.ManyToManyField(Recurso, null=True, blank=True)
 
     def __unicode__(self):
         return self.titulo
